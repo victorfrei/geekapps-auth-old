e682f85429754faba1fff4fb4ab93b78de69ec9f
victorfrei <vic.frei2@gmail.com>
2023-04-12 10:05:20 -0300

    Fix: Homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
-import { UserProfile } from "@clerk/nextjs";
+import { SignInButton, SignOutButton, SignedIn, SignedOut, UserProfile } from "@clerk/nextjs";
 
 const Home = () => {
   return (
     <div className="flex w-screen h-screen justify-center items-center py-10 bg-zinc-800 filter">
-      <UserProfile />
+      <SignedIn >
+        <SignOutButton />
+      </SignedIn>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
     </div>
   );
 };
